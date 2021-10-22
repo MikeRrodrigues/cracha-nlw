bf3d6cdb50e6ef45fd3e98eb12100e8f897ef37c
Mike Rodrigues <71508790+MikeRrodrigues@users.noreply.github.com>
2021-10-22 18:31:46 -0300

    Initial Commit
    Operational files. Images to be commited later.

---
+++ main.js
@@ -0,0 +1,30 @@
+const socialMediaLinks = {
+  youtube: 'channel/UCV31wReKBKpnHANb_T1-j5w',
+  instagram: 'mike_rrodrigues',
+  facebook: 'mike.rodrigues.313',
+  github: 'MikeRrodrigues'
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+    li.children[0].href = `https://${social}.com/${socialMediaLinks[social]}`
+  }
+}
+
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${socialMediaLinks.github}`
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      userImage.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+
+getGitHubProfileInfos()
